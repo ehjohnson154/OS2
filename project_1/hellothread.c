b321f889a390519a50499fed779da9d9cad5c5c3
ehjohnson154 <spiffy154@gmail.com>
2023-04-04 11:57:36 -0700

    finished project 1

--- project_1/hellothread.c
+++ project_1/hellothread.c
@@ -2,13 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <unistd.h>
 
+
+
+//For multiple arguments, internet recommends struct?//
+struct thread_args {
+    int tally;
+    char * thread_name;
+};
 
 
 void *run_func(void *arg){
-    char * name = arg;
-    for (int i = 0; i < 5; i++) {
+    struct thread_args *args = arg;
+    // char * name = arg;
+    // static int num = 0;
+    char * name = args->thread_name;
+    int tally = args->tally;
+
+    for (int i = 0; i < tally; i++) {
     printf("%s: %d\n",name, i);
+    sleep(0);
     }
 
     return NULL;
@@ -19,13 +33,19 @@ int main(void){
     pthread_t t1;
     pthread_t t2;
 
-    printf("Threads running!\n");
+    struct thread_args args1;
+    args1.tally = 5;
+    args1.thread_name = "Thread 1";
+    struct thread_args args2;
+    args2.tally = 5;
+    args2.thread_name = "Thread 2";
 
-    //for (int i = 0; i < 5; i++) {
 
-    pthread_create(&t1, NULL, run_func, "Thread 1");
-    pthread_create(&t2, NULL, run_func, "thread 2");
-    //}
+    printf("Launching threads\n");
+
+    pthread_create(&t1, NULL, run_func, &args1);
+    pthread_create(&t2, NULL, run_func, &args2);
+
 
     pthread_join(t1, NULL);   
     pthread_join(t2, NULL);   
@@ -33,3 +53,4 @@ int main(void){
     printf("threads complete!\n");
 
 }
+
